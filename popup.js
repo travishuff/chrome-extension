8cb7fd08000ba76d3dd50e9abf5ebe6cd898cf7b
David Wilson <w.dave.w@gmail.com>
2016-10-13 02:03:16 -0700

    Handle errors when user tries to add bad listing

--- popup.js
+++ popup.js
@@ -1,49 +1,48 @@
-  console.log('ready');
-  const button = document.getElementById('add-button')
-  const clearButton = document.getElementById('clear-button')
+console.log('ready');
+const button = document.getElementById('add-button')
+const clearButton = document.getElementById('clear-button')
 
-    chrome.storage.sync.get('yelpObjs', (response) => {
-      // console.log(response);
-        if (Object.keys(response).length !== 0) {
-        let bizCard = '';
-        response.yelpObjs.forEach(item => {
-          bizCard += '<div class="row">'
-          bizCard += '<div class="col-pic">'
-          bizCard += '<img class="img-card" src="' + item.imgUrl + '" />'
-          bizCard += '</div>'
-          bizCard += '<div class="col-text">'
-          bizCard += '<div class="name"><a target="_blank" href="' + item.url + '">' + item.title + '</a></div>'
-          bizCard += '<div class="category">' + item.categories + '</div>'
-          bizCard += '<div class="review-count" style="background-position: ' + item.ratingX + ' ' + item.ratingY + '"></div>'
-          bizCard += '<div>' + item.reviewCount + ' Ratings</div>'
-          bizCard += '</div>'
-          bizCard += '<div class="col-del-but">'
-          bizCard += '<button>Delete</button>'
-          bizCard += '</div>'
-          bizCard += '</div>'
-        });
+chrome.storage.sync.get('yelpObjs', (response) => {
+  console.log(response);
+  if (Object.keys(response).length !== 0) {
+    let bizCard = '';
+    response.yelpObjs.forEach(item => {
+      bizCard += '<div class="row">'
+      bizCard += '<div class="col-pic">'
+      bizCard += '<img class="img-card" src="' + item.imgUrl + '" />'
+      bizCard += '</div>'
+      bizCard += '<div class="col-text">'
+      bizCard += '<div class="name"><a target="_blank" href="' + item.url + '">' + item.title + '</a></div>'
+      bizCard += '<div class="category">' + item.categories + '</div>'
+      bizCard += '<div class="review-count" style="background-position: ' + item.ratingX + ' ' + item.ratingY + '"></div>'
+      bizCard += '<div>' + item.reviewCount + ' Ratings</div>'
+      bizCard += '</div>'
+      bizCard += '<div class="col-del-but">'
+      bizCard += '<button>Delete</button>'
+      bizCard += '</div>'
+      bizCard += '</div>'
+    });
 
-        document.querySelector('#list').innerHTML = bizCard;
-        }
-    })
-    
-  // allow user to clear events
-  clearButton.addEventListener('click', function () {
-    chrome.storage.sync.clear(() => console.log('cleared events'));
-    document.querySelector('#list').innerHTML = '';
-  })
-  // when button is clicked, run content script
-  button.addEventListener('click', function () {
-    console.log('clicked');
-  
+    document.querySelector('#list').innerHTML = bizCard;
+  }
+})
 
-    // run content.js and add a card to chrome.storage.sync
-    // requests yelpObjs array 
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
-        // console.log(response.yelpObjects);
-        // create card for each object in yelpObjs
-        
+// allow user to clear events
+clearButton.addEventListener('click', function () {
+  chrome.storage.sync.clear(() => console.log('cleared events'));
+  document.querySelector('#list').innerHTML = '';
+})
+// when button is clicked, run content script
+button.addEventListener('click', function () {
+  console.log('clicked');
+
+  // run content.js and add a card to chrome.storage.sync
+  // requests yelpObjs array 
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
+      // console.log(response.yelpObjects);
+      // create card for each object in yelpObjs
+      if (response) {
         document.querySelector('#list').innerHTML = '';
         let bizCard = '';
 
@@ -63,13 +62,16 @@
           bizCard += '</div>'
           bizCard += '</div>'
         });
-
-
         document.querySelector('#list').innerHTML = bizCard;
-        return true;
 
-      });
+      }
+      else {
+        console.log('You aren\'t on a valid yelp business page');
+      }
+      return true;
+
     });
+  });
 
-  })
-  
+})
+
