101c78c95e5e981c7dfe00ff91712bf9458bc948
travishuff <huff.travis@gmail.com>
2016-10-13 00:47:30 -0700

    Dynamic HTMl rendering

--- popup.js
+++ popup.js
@@ -2,25 +2,71 @@
   const button = document.getElementById('add-button')
   const clearButton = document.getElementById('clear-button')
 
+    chrome.storage.sync.get('yelpObjs', (response) => {
+      // console.log(response);
+        if (Object.keys(response).length !== 0) {
+        let bizCard = '';
+        response.yelpObjs.forEach(item => {
+          bizCard += '<div class="row">'
+          bizCard += '<div class="col-pic">'
+          bizCard += '<img class="img-card" src="' + item.imgUrl + '" />'
+          bizCard += '</div>'
+          bizCard += '<div class="col-text">'
+          bizCard += '<div class="name"><a target="_blank" href="' + item.url + '">' + item.title + '</a></div>'
+          bizCard += '<div class="category">' + item.categories + '</div>'
+          bizCard += '<div class="review-count" style="background-position: ' + item.ratingX + ' ' + item.ratingY + '"></div>'
+          bizCard += '<div>' + item.reviewCount + ' Ratings</div>'
+          bizCard += '</div>'
+          bizCard += '<div class="col-del-but">'
+          bizCard += '<button>Delete</button>'
+          bizCard += '</div>'
+          bizCard += '</div>'
+        });
+
+        document.querySelector('#list').innerHTML = bizCard;
+        }
+    })
+    
   // allow user to clear events
   clearButton.addEventListener('click', function () {
-    chrome.storage.sync.clear(() => console.log('cleared events'))  ;
+    chrome.storage.sync.clear(() => console.log('cleared events'));
+    document.querySelector('#list').innerHTML = '';
   })
   // when button is clicked, run content script
   button.addEventListener('click', function () {
     console.log('clicked');
-
-    chrome.storage.sync.get('yelpObjs', (response) => {
-      console.log(response);
-    })
+  
 
     // run content.js and add a card to chrome.storage.sync
     // requests yelpObjs array 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (response) {
-        console.log(response.yelpObjs);
+        // console.log(response.yelpObjects);
         // create card for each object in yelpObjs
+        
+        document.querySelector('#list').innerHTML = '';
+        let bizCard = '';
 
+        response.yelpObjects.forEach((item, index) => {
+          bizCard += '<div class="row">'
+          bizCard += '<div class="col-pic">'
+          bizCard += '<img class="img-card" src="' + item.imgUrl + '" />'
+          bizCard += '</div>'
+          bizCard += '<div class="col-text">'
+          bizCard += '<div class="name"><a target="_blank" href="' + item.url + '">' + item.title + '</a></div>'
+          bizCard += '<div class="category">' + item.categories + '</div>'
+          bizCard += '<div class="review-count" style="background-position: ' + item.ratingX + ' ' + item.ratingY + '"></div>'
+          bizCard += '<div>' + item.reviewCount + ' Ratings</div>'
+          bizCard += '</div>'
+          bizCard += '<div class="col-del-but">'
+          bizCard += '<button id="">Delete</button>'
+          bizCard += '</div>'
+          bizCard += '</div>'
+        });
+
+
+        document.querySelector('#list').innerHTML = bizCard;
+        return true;
 
       });
     });
